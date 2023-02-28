6712af0b4431c6d0076b8ae4025537b70d6395aa
goodjwon <firstman9@protonmail.com>
2023-03-01 07:55:03 +0900

    보충

--- day1_setup.R
+++ day1_setup.R
@@ -20,11 +20,17 @@ dev.new() # *디바이스를 만든다.
 pie(edu_tb, main="study pie chart")
 dev.off()
 
+
+
 sex_edu = list(survey$sex, survery$edu)
 sex_edu_tb = table(sex_edu)
 sex_edu_tb
+#컬럼에 명칭 주기
+colnames(sex_edu_tb) <- c('none','elem','med', 'high', 'colleage')
+sex_edu_tb
 
 barplot(sex_edu_tb, legend.text = rownames(sex_edu_tb), col=c(2,4))
+title("Stacked Barplot")
 
 hist(survery$salary)
 stem(survery$salary)
@@ -34,3 +40,44 @@ boxplot(salary ~ sex, data=survery)
 
 
 ## day2  two value chart
+plot(co2)
+lines(smooth(co2), col=4)
+write.csv(co2, file='./mva/co2.csv')
+
+
+# Visualization of Multivariate Data
+install.packages("HSAUR2")
+library("HSAUR2")
+install.packages("MVA")
+library(MVA)
+data(USairpollution)
+head(USairpollution, 3)
+
+x = USairpollution[, c(3,4)]
+bvbox(x, xlab='manu', ylab='popul', pch=19)
+title('Bivariate Boxplot')
+dev.new()
+identify(x) #점에 수치 확인. 마우스로 찍으면 나타난다.
+dev.off()
+rownames(x)[c(7,9,14,30)]
+
+plot(wind~temp,data=USairpollution,pch=9)
+
+with(USairpollution, symbols(temp, wind, circle=SO2, inches=0.5, add=T))
+title("Bubble plot")
+
+
+# R 산점도 행렬.
+social = read.table('./mva/social.txt', header = T)
+head(social)
+pairs(social) # 산점도 행렬을 표시한다.
+round(cor(social, use='complete.obs'), 3)
+
+# R 별그림.
+social
+social2 = social[, -1]
+social2
+year = social[,1]
+year
+rownames(social2)<-year
+stars(social2)
