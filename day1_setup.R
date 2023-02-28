a2a1c5a0cecfb7b5e5054a8a0a3af3479c658f09
goodjwon <firstman9@protonmail.com>
2023-02-28 22:31:07 +0900

    day2

--- day1_setup.R
+++ day1_setup.R
@@ -1,3 +1,4 @@
+##day1 *은 중요표시.
 survey = read.csv("./mva/survey.csv")
 head(survey,3)
 
@@ -8,3 +9,28 @@ survey$edu = ordered(survey$edu, levels=c(1:5), labels=c('none','elem','med','hi
 
 table(survey$sex)
 table(survey$edu)
+
+
+## day2  single value chart
+edu_tb = table(survey$edu)
+edu_tb
+rownames(edu_tb) = c('none','elem','med', 'high', 'colleage')
+barplot(edu_tb)
+dev.new() # *디바이스를 만든다.
+pie(edu_tb, main="study pie chart")
+dev.off()
+
+sex_edu = list(survey$sex, survery$edu)
+sex_edu_tb = table(sex_edu)
+sex_edu_tb
+
+barplot(sex_edu_tb, legend.text = rownames(sex_edu_tb), col=c(2,4))
+
+hist(survery$salary)
+stem(survery$salary)
+stem(survery$salary, scale=2)
+
+boxplot(salary ~ sex, data=survery)
+
+
+## day2  two value chart
